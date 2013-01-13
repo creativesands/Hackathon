0df2ae09c6a9c8e34f43f18c223df2197d42a836
Sandeep S Kumar <sanygeek@gmail.com>
2013-01-13 18:16:31 +0000

    Refined Timer object logic and added logic for RSVP section

--- js/main.js
+++ js/main.js
@@ -1,61 +1,116 @@
-var	days_	= $("#days p:last-child"),
-	hours_	= $("#hours p:last-child"),
-	mins_	= $("#mins p:last-child"),
-	secs_	= $("#secs p:last-child");
 
-function Timer(days, hours, mins, secs) {
-	this.days = parseInt(days.text(),10);
-	this.hours = parseInt(hours.text(), 10);
-	this.mins = parseInt(mins.text(), 10);
-	this.secs = parseInt(secs.text(), 10);
-
-	this.consolePrint = function () {
-	    console.log("There are " + this.days + " days and " + this.hours + " hours left for the event.");
-	};
-
-	this.start = function () {
-	    var secs = this.secs;
-	    var mins = this.mins;
-	    var hours = this.hours;
-	    var days = this.days;
+/**
+   Timer object counts down to 0 in days, hours, minutes, seconds and updates corresponding HTML Elements every second.
+   Constructor takes the following parameters:
+   numberOfSeconds :   Total number of seconds to count down from
+   HTML elements for days, hours, mins and seconds
+**/
 
 
-	    var counter = setInterval(function () {
-	        if (days + hours + mins + secs == 0) {
-	            window.clearInterval(counter);
+function Timer(numberOfSeconds, daysTag, hoursTag, minsTag, secsTag) {
+    this.days = days = parseInt((numberOfSeconds / (3600 * 24)),10);
+    this.hours = hours = parseInt((numberOfSeconds / 3600) % 24, 10);
+    this.mins = mins = parseInt(((numberOfSeconds / 60) % 60), 10);
+    this.secs = secs = parseInt((numberOfSeconds % 60), 10);
+
+    //Update HTML with initial values
+    daysTag.text(days);
+    hoursTag.text(hours);
+    minsTag.text(mins);
+    secsTag.text(secs);
+
+    var interval; //reference to stop setInterval()
+    var countingDown = true;
+
+    var updater = function () {
+        if (days + hours + mins + secs == 0 && countingDown) {
+            this.stop();
+	        }
+	    else {
+            if (secs != 0) {
+                secsTag.text(--secs);
 	        }
 	        else {
-                if (secs != 0) {
-	                secs_.text(--secs);
+	            secs = 60;
+	            secsTag.text(--secs);
+	            if (mins != 0) {
+	                minsTag.text(--mins);
 	            }
 	            else {
-	                secs = 60;
-	                secs_.text(--secs);
-	                if (mins != 0) {
-	                    mins_.text(--mins);
+	                mins = 60;
+	                minsTag.text(--mins);
+	                if (hours != 0) {
+	                    hoursTag.text(--hours);
 	                }
 	                else {
-	                    mins = 60;
-	                    mins_.text(--mins);
-	                    if (hours != 0) {
-	                        hours_.text(--hours);
+	                    hours = 24;
+	                    hoursTag.text(--hours);
+	                    if (days != 0) {
+	                        daysTag.text(--days);
 	                    }
 	                    else {
-	                        hours = 24;
-	                        hours_.text(--hours);
-	                        if (days != 0) {
-	                            days_.text(--days);
-	                        }
-	                        else {
-	                            days = 0;
-	                        }
+	                        days = 0;
 	                    }
 	                }
 	            }
 	        }
-	    }, 1000);
-	}
-};
+	    }
+    }
 
-var CDTimer = new Timer(days_,hours_,mins_,secs_);
-CDTimer.start();
+    this.start = function () {
+        countingDown = true;
+        secsTag.addClass("scroll-animation");
+        interval = setInterval(updater,1000);
+    }
+
+    this.stop = function () {
+        countingDown = false;
+        secsTag.removeClass("scroll-animation");
+        window.clearInterval(interval);
+    }
+}
+
+var hackathonTimeStamp = 1358760600000; //Mon, 21 Jan 2013 09:30:00 GMT 1358760600000
+var nowTimeStamp = new Date().getTime(); //Current Timers
+
+if (hackathonTimeStamp > nowTimeStamp) {
+    var timeDiff = hackathonTimeStamp - nowTimeStamp;
+}
+else {
+    var timeDiff = 0;
+}
+
+//Ignoring Milliseconds
+timeDiff = parseInt(timeDiff/1000, 10);
+
+//Reference to HTML elements corresponding to days, hours, mins and secs
+var days_ = $("#days p:last-child"),
+	hours_ = $("#hours p:last-child"),
+	mins_ = $("#mins p:last-child"),
+	secs_ = $("#secs p:last-child");
+
+countDownTimer = new Timer(timeDiff, days_, hours_, mins_, secs_);
+//Start counter only if timeDiff is positive
+if (timeDiff > 0) {
+    countDownTimer.start();
+}
+
+
+/**
+    RSVP LOGIC
+    If user clicks rsvp buttons, hide the buttons and show rsvp-yes message or show rsvp-no message according to the button clicked
+**/
+
+$("#no, #yes").click(function () {
+    $(".rsvp-button-wrapper").fadeOut("fast", function () {
+        $(".rsvp-message").show(); //show message container first
+    });
+});
+
+$("#yes").click(function () {
+    $("#rsvp-yes").slideDown("fast");
+});
+
+$("#no").click(function () {
+    $("#rsvp-no").slideDown("fast");
+});
--- process.php
+++ process.php
@@ -1,5 +1,20 @@
-<?php 
 
-echo "Sand was here!";
+    <?php
+	    /*Calculate difference in time between server time and event time(09:30 IST 21 Jan 2013)*/
+		
+		date_default_timezone_set("Asia/Kolkata");
 
- ?>
+		$days = date("d");
+		$hours = date("H");
+		$mins = date("i");
+		$secs = date("s");
+
+		echo "Days: " . $days . " Hours: " . $hours . " Minutes: " . $mins . " Seconds: " . $secs . "<br />";
+
+		$hackathonTimeStamp = 1358782200;
+		$nowTimeStamp = mktime(date("H"),date("i"),date("s"),date("m"),date("d"),date("Y"));
+
+		print_r(mktime(date("H"),date("i"),date("s"),date("m"),date("d"),date("Y")));
+
+		echo $nowTimeStamp - $hackathonTimeStamp;
+    ?>
